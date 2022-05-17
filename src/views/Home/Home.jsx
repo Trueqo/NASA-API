906e2e84aef7bdc43a22b424b66fc4501fc161e0
Trueqo <julianochoa1999@gmail.com>
2022-05-17 00:19:15 -0500

    home responsive done

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,29 +1,22 @@
-import React from 'react'
-import css from '../Home/Home.module.css'
-import data from '../../data/dataHome'
+import React from "react";
+import css from "../Home/Home.module.css";
+import data from "../../data/dataHome";
 const Home = () => {
-
-
-
-
-
     return (
+        <div className={css.container}>
+            {data.map((cards, id) => (
+                <div className={css.contInfo} key={id}>
+                    <div className={css.card}>
+                        <h1 className={css.title}>{cards.title}</h1>
 
-        <div className={css.container} >
-            {
-                data.map(cards =>
-                    <div className={css.contInfo}>
-                        <div className={css.card}>
-                            <h1 className={css.title}>{cards.title}</h1>
-                            <p className={css.des}>{cards.description}</p>
-                            <h1 className={css.icon}>{cards.icon}</h1>
-                        </div>
+                        <p className={css.des}>{cards.description}</p>
+                        <h1 className={css.icon}>{cards.icon}</h1>
+
                     </div>
-                )
-            }
-
+                </div>
+            ))}
         </div>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
