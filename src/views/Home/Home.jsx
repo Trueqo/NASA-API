ce000af6a3f7a259670faded5c04ecfb9192140b
trueqo <julianochoa1999@gmail.com>
2022-07-18 17:02:52 -0500

    APOD done

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,17 +1,14 @@
 import React from "react";
-import css from "../Home/Home.module.css";
 import data from "../../data/dataHome";
 const Home = () => {
     return (
         <div className="">
             {data.map((cards, id) => (
-                <div className="flex  justify-center  h-[calc(100vh-12rem)] px-[4rem] pt-[4rem] pb-[8rem] relative" key={id}>
-                    <div className="flex flex-col bg-gray-300 items-center p-4 rounded-2xl border border-gray-400">
+                <div className="flex  justify-center  px-[4rem] pt-[4rem] pb-[8rem] relative" key={id}>
+                    <div className="flex flex-col bg-gray-300 items-center rounded-2xl border border-gray-400 px-[1rem]">
                         <h1 className="font-bold text-[3rem]">{cards.title}</h1>
-
                         <p className="text-[2rem]">{cards.description}</p>
                         <h1 className="text-red-600 text-[2rem] ">{cards.icon}</h1>
-
                     </div>
                 </div>
             ))}
